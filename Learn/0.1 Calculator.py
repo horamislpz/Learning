86f5ab0dcffdbd86aa7d53dd0a272c3bb1e13055
horamislpz <161621420+horamislpz@users.noreply.github.com>
2024-02-28 12:08:44 -0500

    0.1 Calculator

---
+++ Learn/0.1 Calculator.py
@@ -0,0 +1,28 @@
+# Learning Python - Calculator
+
+
+
+# Different operations
+# operation = '+' or '*' or '-'
+operation = str(input("Type of operation: + = Sum, - = Minus and * = Factor\n"))
+first_number = float(input("\nType the first number: \n"))
+second_number = float(input("\nType the second number: \n"))
+
+if operation == "+": {
+    first_number + second_number:
+    # Suma
+print ("The sum of the two numbers is:  " + str(float((first_number+second_number))) +"\n")
+} 
+elif operation == "*": {
+    first_number * second_number:
+print ("The factor of the two numbers is:  " + str(float((first_number*second_number))) +"\n")
+}
+
+else: {
+    print("Please type the operation desire\n")
+}
+
+
+
+# Multiplication
+
