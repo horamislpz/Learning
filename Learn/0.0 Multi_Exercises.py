2c5c47f9d4d9d8dad0472d7cf105f1ac7f05ef08
horamislpz <161621420+horamislpz@users.noreply.github.com>
2024-03-01 13:05:47 -0500

    New files

--- Learn/0.0 Multi_Exercises.py
+++ Learn/0.0 Multi_Exercises.py
@@ -39,57 +39,6 @@
 # """
 # print(template.format(name=name, planet=planet, gravity=gravity))
 
-# -----------------------------------------------------------------
-#
-
-# WORKING WITH NUMBERS AND CONVERTING
-
-# from math import ceil, floor
-
-# first_planet_input = input("Enter the distance from the sun for the first planet in km\n")
-# second_planet_input = input("Enter the distance from the sun for the second planet in km\n")
-
-# # converting from string to int
-
-# first_planet = float(first_planet_input)
-# second_planet = float(second_planet_input)
-
-# # calculation
-
-# distance_km = (second_planet - first_planet)
-# print('Distance = ' + str(ceil(distance_km)))
-# print('Distance_float = ' + str(distance_km))
-
-# -----------------------------------------------------------------
-
-# WORKING WITH PYTHON LISTS
-
-
-# planets = ['Mercury', 'Earth', 'Venus', 'Mars', 'Saturn', 'Uranus', 'Neptune', 'Jupiter']
-
-# print('First planet on the list is: ' + planets[0])
-# print('Second planet on the list is: ' + planets[1])
-# print('Third planet on the list is: ' + planets[2])
-
-# planets[1] = 'Tierra'
-# print(planets)
-
-# # Lenght of a list
-# number_of_planets = len(planets)
-# print('Number of planets: ' + str(number_of_planets))
-
-# # Append to list .append(value)
-
-# planets.append('New Planet')
-# new_number_of_planets = len(planets)
-# print("New number of planets: ", new_number_of_planets)
-
-# # finding a value in a list (jupiter)
-# planets_loweredCase = [x.lower() for x in planets]
-# jupiter_find_index = planets_loweredCase.index('jupiter')
-# print('Jupiter is the ', jupiter_find_index + 1, 'planet from the sun')
-
-# --------------------------------------------------------------
 
 # More numbers exercise
 # gravity_on_planets = [0.378, 0.907, 1, 0.377, 2.36, 0.916, 0.889, 1.12]
@@ -102,27 +51,7 @@
 
 # --------------------------------------------------------------
 
-# Slice Lists
-# planets = ['Mercury', 'Earth', 'Venus', 'Mars', 'Saturn', 'Uranus', 'Neptune', 'Jupiter']
-# planets_before_earth = planets[0:1]
-# print(planets_before_earth)
 
-# planets_after_earth = planets[2:]
-# # OR ---> planets_after_earth = planets[2::]
-# print(planets_after_earth)
-
-# --------------------------------------
-# Joining and sorting lists
-# planets = ['Mercury', 'Earth', 'Venus', 'Mars', 'Saturn', 'Uranus', 'Neptune', 'Jupiter']
-# amalthea_group = ["Metis", "Adrastea", "Amalthea", "Thebe"]
-# new_list_joined = planets + amalthea_group
-# print(new_list_joined)
-
-# # Sort list
-# list_to_sort = planets + amalthea_group
-# print('List not sorted: ', list_to_sort)
-# list_to_sort.sort()
-# print('\nSorted list: ', list_to_sort, '\n')
 
 # EXERCISE
 # planets_list = ['Mercury', 'Earth', 'Venus', 'Mars', 'Saturn', 'Uranus', 'Neptune', 'Jupiter']
@@ -137,46 +66,7 @@
 # print('The planet is in position ', planets_index)
 
 #/ --------------------------------------------------------------\
-    # WHILE AND FOR LOOPS
-# WHILE
-
-# person_name = ''
-
-# while person_name=='':
-#     print('Please, complete the information below')
-#     first_name = input('First Name: \n')
-#     last_name = input('Last Name: \n')
-#     person_name = first_name + last_name
-
-# # # # # # # # # # # # # # # # # # # # # 
-
-# Creating list with user inputs
-
-# names = ''
-# name_list = [] 
-
-# while names.lower() != 'done':
-#     if names:
-#         name_list.append(names)
-#     names = input("Please provide the list of names or 'done' when done\n")
-#     print(name_list)
-
-
-#     # FOR
-
-# countdown = [4,3,2,1,0]
-# for number in countdown:
-#     print(number)
-# print("Blast off!! 🚀")
-
-# # Waiting a second for every iteration
-# from time import sleep
-# countdown = [4,3,2,1,0]
-# for number in countdown:
-#     print(number)
-#     sleep(1)
-# print("Blast off!! 🚀")
-
+        
 ####   EXERCISE   ####
 # from time import sleep
 # from traceback import print_list
