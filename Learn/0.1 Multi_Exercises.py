fa3e2efd315dea6dd10b38b29a74ca45063093a8
horamislpz <161621420+horamislpz@users.noreply.github.com>
2024-02-29 20:30:31 -0500

    MORE CHANGES

--- Learn/0.1 Multi_Exercises.py
+++ Learn/0.1 Multi_Exercises.py
@@ -187,3 +187,97 @@
 
 # --------------------------------------------------------------
 
+# Slice Lists
+# planets = ['Mercury', 'Earth', 'Venus', 'Mars', 'Saturn', 'Uranus', 'Neptune', 'Jupiter']
+# planets_before_earth = planets[0:1]
+# print(planets_before_earth)
+
+# planets_after_earth = planets[2:]
+# # OR ---> planets_after_earth = planets[2::]
+# print(planets_after_earth)
+
+# --------------------------------------
+# Joining and sorting lists
+# planets = ['Mercury', 'Earth', 'Venus', 'Mars', 'Saturn', 'Uranus', 'Neptune', 'Jupiter']
+# amalthea_group = ["Metis", "Adrastea", "Amalthea", "Thebe"]
+# new_list_joined = planets + amalthea_group
+# print(new_list_joined)
+
+# # Sort list
+# list_to_sort = planets + amalthea_group
+# print('List not sorted: ', list_to_sort)
+# list_to_sort.sort()
+# print('\nSorted list: ', list_to_sort, '\n')
+
+# EXERCISE
+# planets_list = ['Mercury', 'Earth', 'Venus', 'Mars', 'Saturn', 'Uranus', 'Neptune', 'Jupiter']
+# # making every word in lowercase
+# planets = [x.lower() for x in planets_list]
+# user_planet_input = input('Please enter the name of the planet\n\t')
+# user_planet_input.lower()
+
+# # searching the position of the planet introduced by the user
+
+# planets_index = planets.index(user_planet_input)
+# print('The planet is in position ', planets_index)
+
+#/ --------------------------------------------------------------\
+    # WHILE AND FOR LOOPS
+# WHILE
+
+# person_name = ''
+
+# while person_name=='':
+#     print('Please, complete the information below')
+#     first_name = input('First Name: \n')
+#     last_name = input('Last Name: \n')
+#     person_name = first_name + last_name
+
+# # # # # # # # # # # # # # # # # # # # # 
+
+# Creating list with user inputs
+
+# names = ''
+# name_list = [] 
+
+# while names.lower() != 'done':
+#     if names:
+#         name_list.append(names)
+#     names = input("Please provide the list of names or 'done' when done\n")
+#     print(name_list)
+
+
+#     # FOR
+
+# countdown = [4,3,2,1,0]
+# for number in countdown:
+#     print(number)
+# print("Blast off!! 🚀")
+
+# # Waiting a second for every iteration
+# from time import sleep
+# countdown = [4,3,2,1,0]
+# for number in countdown:
+#     print(number)
+#     sleep(1)
+# print("Blast off!! 🚀")
+
+####   EXERCISE   ####
+# from time import sleep
+# from traceback import print_list
+
+# names = ''
+# names_list = []
+
+# while names.lower() != 'done':
+#     if names:
+#         names_list.append(names)
+#     names = input("Please provide a list of names, and 'done' when done\n")
+#     #print(names_list)
+    
+# for names in names_list:
+#         print(*names) #PRINTS IN SPACES EACH CHARACTER.
+#         sleep(1)
+
+  
+    
