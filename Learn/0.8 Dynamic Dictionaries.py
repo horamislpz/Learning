458de414cef2df090b285bb97d5a4401c58f671f
horamislpz <161621420+horamislpz@users.noreply.github.com>
2024-03-05 13:31:58 -0500

    Dynamic Programming

--- Learn/0.8 Dynamic Dictionaries.py
+++ Learn/0.8 Dynamic Dictionaries.py
@@ -1,1 +1,26 @@
-#New File
+            # Dynamic Dictionaries
+            
+# To print every Key in a Dictionary
+
+rainfall = {
+    'november'.title() : 3.4,
+    'december': 2.6,
+    'january' : 8.9
+}
+for key in rainfall.keys():
+    print(f'{key.title()}:{rainfall[key]} cm')
+print('---------------------------------------------\n')
+
+# Determine if a key exists in a dictionary
+
+if 'may' in rainfall:
+    rainfall['may'] = 9.8
+else:
+    rainfall.update({'may': 9.8})
+print(rainfall)
+print('\n')
+
+if 'may' in rainfall:
+    rainfall['may'] = rainfall['may'] + 156.9/3
+
+print(rainfall)
