218c976fcc2e599117923bdf2e72ea4453dcb28f
Horamis Lopez <161621420+horamislpz@users.noreply.github.com>
2024-03-06 14:27:26 -0500

    Delete Learn directory

--- Learn/1.0 Keyword Arguments.py
+++
@@ -1,8 +0,0 @@
-        # Keyword Arguments - 
-        
-from datetime import date, timedelta, datetime
-
-def arrival_time(hours=51):
-    now = datetime.now()
-    arrival = now + timedelta(hours=hours)
-    return arrival.strftime("Arrival: %A %H:%M")
